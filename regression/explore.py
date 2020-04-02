dc312faa46b2739896435ca976f2be20df546525
Ravinder Singh <ravinder79@gmail.com>
2020-04-02 17:12:41 -0500

    explore

--- regression/explore.py
+++ regression/explore.py
@@ -11,15 +11,18 @@ def plot_variable_pairs(df):
 
 def months_to_years(tenure_months, df):
     """returns the dataframe with a new feature tenure_years"""
-    df['tenure_years'] = round(tenure_months/12)
+    df['tenure_years'] = (tenure_months//12)
     return df
 
 def plot_categorical_and_continous_vars(categorical_var, continuous_var, df):
     """outputs 3 different plots for plotting a categorical variable with a continuous variable"""
-    f, axes = plt.subplots(1, 2, sharey=True, figsize=(6, 4))
+    #f, axes = plt.subplots(1, sharey=True, figsize=(6, 4))
     plt.figure()
-    sns.boxplot(x= categorical_var, y= continuous_var, data=df, ax=axes[0])
+    plt.figure(figsize=(12,6))
+    sns.boxplot(x= categorical_var, y= continuous_var, data=df)
     plt.figure()
+    plt.figure(figsize=(12,6))
     sns.swarmplot(x= categorical_var, y= continuous_var, data=df)
     plt.figure()
+    plt.figure(figsize=(12,6))
     sns.barplot(x= categorical_var, y= continuous_var, data=df)
