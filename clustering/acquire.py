64d3649eedfc7be7423851dee77d47319bcd18dc
Ravinder Singh <ravinder79@gmail.com>
2020-05-01 17:02:58 -0500

    cleaned up

--- clustering/acquire.py
+++ clustering/acquire.py
@@ -21,14 +21,32 @@ def get_zillow_data():
     WHERE properties_2017.latitude IS NOT NULL OR  properties_2017.longitude IS NOT NULL
     """
     df = pd.read_sql(query, get_db_url('zillow'))
+    # Drop one of the duplicate column named id
+    df = pd.concat([
+    df.iloc[:, :11], # all the rows, and up to, but not including the index of the column to drop
+    df.iloc[:, 11 + 1:] # all the rows, and everything after the column to drop
+    ], axis=1)
+    #sort values by parcelid and transactiondate
+    df = df.sort_values(["parcelid", "transactiondate"])
+
+    # drop duplicates
+    df = df.drop_duplicates(subset=["parcelid"],keep='last')
     return df
 
+def rows_missing(df):
+    df1 = pd.DataFrame(df.isnull().sum(), columns = ['num_rows_missing'])
+    df1['pct_rows_missing'] = df1.num_rows_missing/df.shape[0]
+    return df1  
 
-def get_iris_data():
-    query = """
-    SELECT * 
-    FROM `measurements`
-    JOIN species USING (species_id)
-    """
-    df = pd.read_sql(query, get_db_url("iris_db"))
-    return df
+def cols_missing(df):
+    """function that takes in a dataframe of observations and attributes and returns a dataframe where each row is
+    an atttribute name, the first column is the number of rows with missing values for that attribute, 
+    and the second column is percent of total rows that have missing values for that attribute"""
+    df['total'] = df.apply(lambda row: row.isnull().sum(), axis =1)
+    df2 = df[['total']]
+    df2 = df2.reset_index()
+    df2 = df2.groupby('total').count()
+    df2= df2.reset_index()
+    df2.rename(columns = {'total': 'num_cols_missing', 'index': 'num_rows'}, inplace = True)
+    df2['pct_cols_missing'] = df2.num_cols_missing/df.shape[1]
+    return df2
