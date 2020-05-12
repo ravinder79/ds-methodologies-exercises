c4b5176cf6663411cb70bb56a21e24b2b1b93312
Ravinder Singh <ravinder79@gmail.com>
2020-05-12 15:49:43 -0500

    first commit

---
+++ time_series/prepare.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+from datetime import timedelta, datetime
+import warnings
+warnings.filterwarnings("ignore")
+import matplotlib.pyplot as plt
+
+
+def read_convert_datetime():
+    """ Read Data from CSV and set datetime as index and parse dates columns"""
+    df = pd.read_csv('all_data.csv',index_col = 0, parse_dates = True)
+    df.sale_date = pd.to_datetime(df.sale_date, format='%a, %d %b %Y %H:%M:%S %Z')
+    return df
+
+def set_index_store(df):
+    df = df.sort_values('sale_date').set_index('sale_date')
+    return df
+
+def add_month_week(df):
+    df['day_of_week'] = df.index.day_name()
+    df['month'] = df.index.month
+    return df
+
+def sales_total(df):
+    df['sales_total'] = df.sale_amount * df.item_price
+    return df
+
+def sales_diff(df):
+    df['sales_diff'] = df.sales_total.diff(1)
+    return df
+
+def prepare_store_data():
+    df = read_convert_datetime()
+    df = set_index_store(df)
+    df = add_month_week(df)
+    df = sales_total(df)
+    df = sales_diff(df)
+    return df
+
+
+## OPS German data 
+
+def read_convert_DT():
+    power = pd.read_csv('opsd_germany_daily.csv', index_col=0)
+    power['Date'] = pd.to_datetime(power['Date'])
+    return power
+
+def set_index_ops(df):
+    df = df.sort_values('Date').set_index('Date')
+    return df
+
+def add_month_year(df):
+    df['month'] = df.index.month
+    df['year'] = df.index.year
+    return df
+
+def prepare_ops_data():
+    power = read_convert_DT()
+    power = set_index_ops(power)
+    power = add_month_year(power)
+    return power
